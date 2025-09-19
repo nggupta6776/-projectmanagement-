bd7f474c7c2445664722945f76a286055fd61bcf
Nishant gupt <luxury_curable1 87@slmail.me87@slmail.me>
2025-09-20 01:07:28 +0530

    Initial commit

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("Start of backend project");
+import dotenv from "dotenv";
+import app from "./src/app.js"
+import connectDB from "./src/db/index.js";
+
+dotenv.config({
+    path: "./.env",
+});
+
+
+const port = process.env.PORT || 4000;
+
+
+
+connectDB()
+.then(() => {
+    app.listen(port, () => {
+        console.log(`example app listening on port http://localhost:${port}`);
+    });
+})
+.catch(err => {
+    console.error("MongoDB connection error",err)
+    process.exit(1)   
+})
